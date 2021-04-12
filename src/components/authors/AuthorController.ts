d5d7163be20c8f80ffef5f95ac3542a6083f48d2
Ridwan Taiwo <donriddo@gmail.com>
2021-04-12 01:26:04 +0100

    implement authors and acronyms APIs

---
+++ src/components/authors/AuthorController.ts
@@ -0,0 +1,100 @@
+import { controller, post } from 'route-decorators';
+import { Request, Response, NextFunction } from 'express';
+import * as createError from 'http-errors';
+
+import AuthorService from './AuthorService';
+import { generateApiKey, validate } from '../../utils/app';
+
+@controller('/authors')
+export default class AuthorController {
+  @post('/register')
+  public async registerAuthor(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+
+    try {
+      this.validate(req.body);
+      const author = await AuthorService.createOne(req.body);
+
+      return res.status(200).json({
+        message: 'Registration successful. Please keep your API key safe. It will only be shown to you once',
+        data: {
+          ...author,
+          apiKey: author.apiKey,
+        },
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  @post('/generate-api-key')
+  public async generateNewApiKey(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+
+    try {
+      this.validate(req.body, 'generate-api-key');
+      const author = await AuthorService.findOne({
+        email: req.body.email,
+      });
+
+      if (!author) {
+        throw createError(404, 'Email not registered');
+      }
+
+      const apiKey = generateApiKey(author.email);
+      await AuthorService.update({ email: req.body.email }, { apiKey });
+
+      return res.status(200).json({
+        message: 'Please keep your API key safe. It will only be shown to you once',
+        data: {
+          apiKey,
+        },
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  private validate(body, type = 'register') {
+    let fields = {};
+
+    if (type === 'register') {
+      fields = {
+        email: {
+          type: 'email',
+          required: true,
+        },
+        name: {
+          type: 'string',
+        },
+      };
+    }
+
+    if (type === 'generate-api-key') {
+      fields = {
+        email: {
+          type: 'email',
+          required: true,
+        },
+      };
+    }
+
+    validate(
+      body,
+      {
+        properties: fields,
+      },
+      {
+        strictRequired: true,
+        unknownProperties: 'delete',
+        trim: true,
+      },
+    );
+  }
+}
