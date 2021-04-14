3216bb4a98b3cd12e5c88a27d92be60c10500b85
Ridwan Taiwo <donriddo@gmail.com>
2021-04-14 21:02:19 +0100

    refactor error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,30 +1,7 @@
-import { omit, startCase } from 'lodash';
-import * as pluralize from 'pluralize';
+import { NextFunction, Response, Request } from 'express';
+import { omit } from 'lodash';
 
-export default function errorHandler(err, req, res, next) {
-  if (err.code === 'ENOTFOUND') {
-    return res.status(500).send({
-      message: 'Service not available at the moment. Please try again later',
-    });
-  }
-
-  if (err.name === 'SequelizeUniqueConstraintError') {
-    return res.status(400).send({
-      message: err.errors[0].message.replace('must be unique', 'already exist'),
-    });
-  }
-
-  if (err.code === 11000) {
-    const vars = err.message.split(':');
-    const tableName = vars[1].split(' ')[1].split('.')[1];
-    const modelName = startCase(pluralize.singular(tableName));
-    const fieldName = vars[2].split(' ')[1].split('_')[0];
-
-    return res.status(400).json({
-      message: `${modelName} with the ${fieldName} exists`,
-    });
-  }
-
+export default function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
   if (err.message.includes('Cast to ObjectId failed')) {
     return res.status(400).json({
       message: 'Invalid request parameter sent',
@@ -45,6 +22,4 @@ export default function errorHandler(err, req, res, next) {
   if (err) {
     return res.status(err.status).send({ message: err.message, errors: omit(err, ['response']) });
   }
-
-  res.status(404).json({ message: 'Not Found' });
 }
