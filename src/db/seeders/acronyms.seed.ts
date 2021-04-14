377f4ada6294fa36fd0097faadf6c1822da5751f
Ridwan Taiwo <donriddo@gmail.com>
2021-04-14 13:42:32 +0100

    handle error thrown by seedAcronyms

--- src/db/seeders/acronyms.seed.ts
+++ src/db/seeders/acronyms.seed.ts
@@ -4,27 +4,31 @@ import AuthorService from '../../components/authors/AuthorService';
 const acronyms = require('./acronym.json');
 
 export default async function seedAcronyms() {
-  const hasBeenSeeded = await AcronymService.findOne({
-    key: Object.keys(acronyms[0])[0],
-  });
+  try {
+    const hasBeenSeeded = await AcronymService.findOne({
+      key: Object.keys(acronyms[0])[0],
+    });
 
-  if (hasBeenSeeded) return;
+    if (hasBeenSeeded) return;
 
-  let admin = await AuthorService.findOne({
-    email: 'author@admin.user',
-  });
-
-  if (!admin) {
-    admin = await AuthorService.createOne({
+    let admin = await AuthorService.findOne({
       email: 'author@admin.user',
     });
+
+    if (!admin) {
+      admin = await AuthorService.createOne({
+        email: 'author@admin.user',
+      });
+    }
+
+    const data = acronyms.map((acronym) => ({
+      key: Object.keys(acronym)[0],
+      value: Object.values(acronym)[0],
+      author: admin.id,
+    }));
+
+    return await AcronymService.createMany(data);
+  } catch (error) {
+    console.log('Error seeding acronyms: ', error);
   }
-
-  const data = acronyms.map((acronym) => ({
-    key: Object.keys(acronym)[0],
-    value: Object.values(acronym)[0],
-    author: admin.id,
-  }));
-
-  return await AcronymService.createMany(data);
 }
