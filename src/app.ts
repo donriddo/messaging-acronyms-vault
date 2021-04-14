2d871f95d23a7c2301e34d180ee73ea306c254c4
Ridwan Taiwo <donriddo@gmail.com>
2021-04-14 21:41:11 +0100

    register morgan middleware

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import * as express from 'express';
 import * as cors from 'cors';
+import * as morgan from 'morgan';
 
 import { generatedRoutes } from './api';
 import errorHandler from './middlewares/errorHandler';
+import { loggerStream } from './utils/app';
+import * as config from '../config';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '5mb', type: 'application/json' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
+
+const isDevOrTest = ['development', 'test'].indexOf(config.get('env')) > -1;
+const logType = isDevOrTest ? 'dev' : 'combined';
+app.use(morgan(logType, { stream: loggerStream }));
+
 app.use('/api/', generatedRoutes());
 
 app.use('*', (req, res, next) => {
